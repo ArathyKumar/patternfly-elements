5d5c3ff0faa7185807f13151561ef8e1ce06c2b6
Arathy-s <ask6295@gmail.com>
2024-11-17 22:26:22 +0530

    chore: radio group added

--- elements/pf-radio-group/pf-radio-group.ts
+++ elements/pf-radio-group/pf-radio-group.ts
@@ -3,6 +3,7 @@ import { customElement } from 'lit/decorators/custom-element.js';
 import './pf-radio.js';
 
 import styles from './pf-radio-group.css';
+import { property } from 'lit/decorators/property.js';
 
 /**
  * Radio Group
@@ -11,17 +12,34 @@ import styles from './pf-radio-group.css';
 @customElement('pf-radio-group')
 export class PfRadioGroup extends LitElement {
   static readonly styles: CSSStyleSheet[] = [styles];
+  @property({ attribute: 'radio-group-name' }) radioGroupName = 'contact';
+  @property({ attribute: 'radio-input' }) radioInput?: any = [
+    {
+      label: 'Email',
+      value: 'email',
+      id: 'contactChoice1',
+    }, {
+      label: 'Phone',
+      value: 'phone',
+      id: 'contactChoice2',
+    }];
 
   render(): TemplateResult<1> {
     return html`
-      <div>
-        <pf-radio></pf-radio>
-        <pf-radio></pf-radio>
-
-        <section>
-          <input name="radio" type="radio"/>
-          <input name="radio" type="radio"/>
-        </section>
+      <div class="pf-c-radio-group">
+        ${this.radioInput?.map((radio: any) => {
+          return html`
+            <div class="pf-c-radio">
+              <input
+                type="radio"
+                id=${radio.id}
+                name=${this.radioGroupName}
+                value=${radio.value}
+              />
+              <label class="pf-c-radio--label" for=${radio.id}>${radio.label}</label>
+            </div>
+          `;
+        })}
       </div>
     `;
   }
